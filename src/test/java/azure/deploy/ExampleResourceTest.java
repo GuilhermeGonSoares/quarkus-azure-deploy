b74e8bf7cbeeb74f60a4c27d157a6b240f957b3f
Guilherme Gonçalves <guilherme16.gon@gmail.com>
2024-07-30 23:49:17 -0300

    package uber fat

---
+++ src/test/java/azure/deploy/ExampleResourceTest.java
@@ -0,0 +1,19 @@
+package azure.deploy;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+class ExampleResourceTest {
+    @Test
+    void testHelloEndpoint() {
+        given()
+          .when().get("/")
+          .then()
+             .statusCode(200);
+    }
+
+}
